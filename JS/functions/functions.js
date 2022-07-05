b157132889378c8d760bc959675f9ee37702222c
Deivison-Almeida <deivisonalmeida.eng@gmail.com>
2022-07-05 14:19:44 -0300

    added functionshtml

---
+++ JS/functions/functions.js
@@ -0,0 +1,39 @@
+/* PRIMEIRA PARTE
+
+
+//criar um aplicativo de frases motivacionais
+
+//decraration - declaração da função -> function statement
+
+function createPhrases() {
+    console.log('Estudar é muito bom')
+    console.log('Quem morre no final organiza o funeral')
+    console.log('Um dia eu viro desenvolvedor sênior')
+}
+
+//chamar a função (execute, run, call, invoke)
+
+createPhrases()
+createPhrases()
+createPhrases()
+
+
+
+console.log('fim do programa')
+
+FIM DA PRIMEIRA PARTE*/
+
+
+
+
+
+//function anonymous
+
+const sum = function(num1, num2) {
+    console.log(num1+num2)
+
+}
+
+sum(2,5)
+sum(345,655)
+sum(234,77)
