0d6c3ee73ba289ca3b910b3d51c2bd04a0aa7d66
Deivison-Almeida <deivisonalmeida.eng@gmail.com>
2022-07-19 09:13:23 -0300

    Sem Modal

---
+++ HTML/rocketseat/Dom/dom.js
@@ -0,0 +1,12 @@
+//getElementById
+console.log(document.getElementById('blog-tittle'))
+
+//getElementByClassName
+console.log(document.getElementsByClassName('subtittle'))
+
+//getElementByTagName
+console.log(document.getElementsByTagName('p'))
+
+//querySelector
+
+console.log(document.querySelector(''))
